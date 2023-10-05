10f2e9f1f6fb25f5e25e2d288f6261972dfbd5ff
riorio805 <sefrianojieftara@gmail.com>
2023-10-05 15:48:04 +0700

    Optimize make-katalog all books loaded in ~8s

--- katalog/views.py
+++ katalog/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from django.http import HttpResponse
 from proyekts.settings import BASE_DIR
 import os
+import time
 # Create your views here.
 
 # USER GAK BOLEH AKSES
@@ -11,21 +12,35 @@ def make_book_dataframe(request):
     source= os.path.join(BASE_DIR, 'datasets')
     res=""
     book_dataset = pd.read_csv(source+'\\pg_catalog.csv', index_col="Text#", dtype=object)
-    for i in range(100):
-        name = book_dataset.iloc[[i]]['Title']
+    
+    print(f"{len(book_dataset)} books test:")
+
+    tm = time.time()
+
+    book_list = []
+    printed = True
+    for row in book_dataset.itertuples():
+        name = row.Title
         name = str(name)
         name.replace("\n", " ")
         name.replace("\r", " ")
         book = Book(
-            title=name[5:],
-            author = book_dataset.iloc[[i]]['Authors'],
-            subject = book_dataset.iloc[[1]]['Subjects']
+            title=name,
+            author = row.Authors,
+            subject = row.Subjects
         )
-        book.save()
-        print(book)
-        res+=str(book)+"\n"
+        book_list.append(book)
 
-    return HttpResponse(res)
+        res+=str(book)+"\n"
+    
+    print(f"pandas took {time.time() - tm} seconds")
+    tm = time.time()
+
+    Book.objects.bulk_create(book_list)
+
+    print(f"django took {time.time() - tm} seconds")
+
+    return HttpResponse(res, content_type="text/plain")
     
 def search_book(request, searched_book):
     all_books = Book.objects.all()
@@ -64,3 +79,5 @@ def get_books(request):
     books = Book.objects.all()
     for b in books:
         print(b)
+
+    return HttpResponse("OK")
