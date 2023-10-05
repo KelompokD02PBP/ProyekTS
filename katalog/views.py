9b5a2277c00eed8db6b7021f76430f02feba6326
KenKomKom <119410845+KenKomKom@users.noreply.github.com>
2023-10-05 23:36:11 +0700

    SEARCH with session as memo

--- katalog/views.py
+++ katalog/views.py
@@ -49,6 +49,14 @@ def make_book_dataframe(request):
 
 
 def search_book(request, searched_book):
+    tm = time.time()
+    if 'last_searched' in request.session:
+        if searched_book.lower() in request.session['last_searched'].keys():
+            print(f"searchmemo {searched_book} took {time.time() - tm} seconds")
+            return HttpResponse(request.session['last_searched'][searched_book.lower()])
+    else:
+        request.session['last_searched']={}
+
     all_books = Book.objects.all()
     all_books_name = [(str(b.title).lower(), str(b.author).lower()) for b in all_books]
     res=set()
@@ -61,20 +69,28 @@ def search_book(request, searched_book):
     regex_to_search_end = rf".*{searched_book}^"
 
     index = 0
-    tm = time.time()
+
     for tuple in all_books_name:
 
-        match = bool(re.search(regex_to_search, tuple[0])) or bool(re.search(regex_to_search, tuple[1]))\
+        match = searched_book in tuple[0] or searched_book in tuple[1] or bool(re.search(regex_to_search, tuple[0])) or bool(re.search(regex_to_search, tuple[1]))\
                 or bool(re.search(regex_to_search_start, tuple[0])) or bool(re.search(regex_to_search_start, tuple[1]))\
                 or bool(re.search(regex_to_search_end, tuple[0])) or bool(re.search(regex_to_search_end, tuple[1]))
         if(match):
             res.add(all_books[index])
         index+=1
-    print(f"search took {time.time() - tm} seconds") #12 second
+    print(f"search {searched_book} took {time.time() - tm} seconds") #12.358 second #10.57
     found_books = ""
 
     for b in res:
         found_books+=str(b)+"\n"
+
+    dic = request.session['last_searched']
+    dic[searched_book]=found_books
+
+    request.session.modified=True
+
+    # print(request.session['last_searched'])
+    
     return HttpResponse(found_books)
     
 def get_books(request):
