e0a835717a2fc15bfa011d7f9bfd73739d7efd96
shabhias <shabhialiyya3101@gmail.com>
2023-10-24 20:00:35 +0700

    model

--- main/models.py
+++ main/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 class Like(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     book = models.ForeignKey(to=Book, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return str(self.user.id)+"likes"+str(self.book.title)
