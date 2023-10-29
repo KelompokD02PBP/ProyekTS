d21d880e607b9ac13cd289ae179acaf222cb9dea
shabhias <shabhialiyya3101@gmail.com>
2023-10-29 13:58:30 +0700

    final

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound, JsonResponse
 from django.urls import reverse
 
 from katalog.models import Book
@@ -20,6 +21,7 @@ from django.core.validators import validate_email
 from django.contrib.auth.models import User
 
 from django.views.decorators.csrf import csrf_exempt
+from .models import Like
 
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = 1000000
@@ -37,7 +39,7 @@ def show_main(request):
 
         if request.user.id !=None: #Kalo udh login bisa liat halaman
             context['page_num']=1
-            context['likes']=get_liked_books(request.user)
+            # context['likes']=get_liked_books(request.user)
             context['user_id']=request.user.pk
             context['books']=get_katalog(1,order_by=request.GET.get('order_by'))
 
@@ -195,15 +197,6 @@ def get_katalog(page_num,order_by):
         i+=1
 
     return result
-
-def get_liked_books(user) :
-    likes = []
-    if user:
-        # Mengambil book yang telah dilike oleh user
-        liked_books = Like.objects.filter(user = user).order_by('-timestamp')
-        for like in liked_books: likes.append(like.book)
-    
-    return likes
 
 '''
 membuat daftar yang dicari
@@ -446,3 +439,20 @@ def sort_main_ajax_search(request,page_num):
     sorted_books = search_katalog(last_searched, page_num,order_by)
     
     return HttpResponse(serializers.serialize('json',sorted_books))
+
+# Function untuk mendapatkan data buku yang sudah dilike
+@csrf_exempt
+def get_liked_books_ajax(request):
+    if request.method == 'POST':
+        books = Like.objects.filter(user = request.user).order_by('-timestamp')
+        
+        books = [like.book for like in books]
+
+        # Serialisasi data buku ke dalam format JSON
+        serialized_books = serializers.serialize('json', books)
+
+        # Kirim data buku sebagai respons JSON
+        return JsonResponse({'status': 'success', 'books': serialized_books})
+
+    # Jika metode permintaan tidak valid
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method.'})
